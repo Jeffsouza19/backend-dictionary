fa31907c63ec90e5b18f9c7719fffd0f0979c063
Jeferson <jeffsouza.edif@gmail.com>
2024-10-17 23:43:07 -0300

    criando rotas e controllers

---
+++ backend-dictionary/app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\SignInRequest;
+use App\Http\Requests\SignUpRequest;
+
+class AuthController extends Controller
+{
+    public function signin(SigninRequest $request)
+    {
+
+    }
+
+    public function signup(SignupRequest $request)
+    {
+
+    }
+}
